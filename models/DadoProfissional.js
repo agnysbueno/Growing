41784f68125772618f8ed0975143093238489844
unknown <Digital@Comodato.dhbrasil.digitalhouse.com>
2020-05-14 16:16:47 -0300

    criando model DadoProfissional

---
+++ models/DadoProfissional.js
@@ -0,0 +1,59 @@
+const DadoProfissional = (sequelize, DataTypes) => {
+    let dadoProfissional = sequelize.define(
+        'DadoProfissional',
+        {
+            id: {
+              type: Sequelize.INTEGER,
+              allowNull: false,
+              primaryKey: true,
+              autoIncrement: true
+            },
+            cnpj: {
+              type: Sequelize.STRING(18),
+              allowNull: false
+            },
+            razao_social: {
+              type: Sequelize.STRING(150),
+              allowNull: true
+            },
+            nome_fantasia: {
+              type: Sequelize.STRING(80),
+              allowNull: true
+            },
+            inscricao_estadual: {
+              type: Sequelize.STRING(14),
+              allowNull: false,
+            },
+            inscricao_municipal: {
+              type: Sequelize.STRING(14),
+              allowNull: true
+            },
+            fk_usuario: {
+              type: Sequelize.INTEGER,
+              references: {
+                model: 'usuario',
+                key: 'id'
+              },
+              onUpdate: 'CASCADE',
+              onDelete: 'CASCADE',
+              allowNull: false
+            }
+        }, 
+        { 
+            tableName: "dado_profissional",
+            timestamps: false 
+        }
+    );
+
+    dadoProfissional.associate = (models) => {
+        
+        dadoProfissional.belongsTo(models.Usuario, {
+            foreignKey:'fk_usuario', as: 'usuario'
+        });
+
+    };
+
+    return dadoProfissional;
+}
+
+module.exports = DadoProfissional;
