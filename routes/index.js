a700685c4bdcf9b604f7bac9baa106a59eb51a6d
cloudsystems22 <cloudsystems22@gmail.com>
2020-05-16 08:59:25 -0300

    Instalação e configuração do multer e do session

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const multer = require('multer');
+const path = require('path');
+//Configurado para subir imagens
+//Acrecentar 'uploadImagens.any', antes das controlers
+let imagens = multer.diskStorage({
+    destination:(req, file, cb) =>{
+        cb(null, path.join('public', 'images'));
+    },
+    filename:(req, file, cb) =>{
+        cb(null, file.originalname);
+    }
+});
+let uploadImagens = multer({iamgens:imagens});
+
+
+//Controllers
 const homeController = require('../controllers/homeController');
 
 /* GET home page. */
