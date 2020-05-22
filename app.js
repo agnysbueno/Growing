2ddc6e43d1ef0e77e4f335861e54f33dce6e0714
unknown <Digital@Comodato.dhbrasil.digitalhouse.com>
2020-05-22 18:39:58 -0300

    adicionando postsRouter no app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var postsRouter = require('./routes/posts');
 var produtosRouter = require('./routes/produtos');
 var servicosRouter = require('./routes/servicos');
 
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+app.use('/posts', postsRouter);
 app.use('/produtos', produtosRouter);
 app.use('/servicos', servicosRouter);
 
