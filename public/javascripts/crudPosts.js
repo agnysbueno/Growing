e311447dee475f25aed4dd93aa27e5954bd74a59
cloudsystems22 <cloudsystems22@gmail.com>
2020-06-04 12:41:42 -0300

    Funçao publicImagem

--- public/javascripts/crudPosts.js
+++ public/javascripts/crudPosts.js
@@ -6,4 +6,16 @@ btnCriarPublic.addEventListener('click', function() {
 
 btnfchPublic.addEventListener('click', function() {
     document.getElementById('janela-publicar').classList.toggle('active');
-})
+});
+
+function publicImag(){
+    let janelaPublicar = document.getElementById('janela-publicar');
+    janelaPublicar.classList.toggle('imagem');
+    let imagePost = document.createElement('img');
+    imagePost.setAttribute('id', 'imagePost');
+    imagePost.setAttribute('src', './images/servicos/mascara-de-hidratacao.jpg');
+
+    let divimgPost = document.getElementById('divimgPost');
+    divimgPost.appendChild(imagePost);
+
+}
