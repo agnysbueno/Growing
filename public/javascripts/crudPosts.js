3012b89decd89ada9f8d7167b8e7076ed0e9daac
cloudsystems22 <cloudsystems22@gmail.com>
2020-06-04 12:04:40 -0300

    Janela para postagens js + rota

---
+++ public/javascripts/crudPosts.js
@@ -0,0 +1,9 @@
+let btnCriarPublic = document.getElementById('btnCriarPublic');
+
+btnCriarPublic.addEventListener('click', function() {
+    document.getElementById('janela-publicar').classList.toggle('active');
+});
+
+btnfchPublic.addEventListener('click', function() {
+    document.getElementById('janela-publicar').classList.toggle('active');
+})
